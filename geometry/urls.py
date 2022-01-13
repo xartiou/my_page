95823d84a5868ae5e4f49cc6389d4d61730b5961
xartiou <xartiou@gmail.com>
2022-01-13 17:28:01 +0300

    в приложении geometry создал шаблоны rectangle.html, square.html, circle.html

--- geometry/urls.py
+++ geometry/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('rectangle/<int:width>/<int:height>', views.rectangle_area, name='rectangle_area'),
+    path('rectangle', views.rectangle),
     path('square/<int:width>', views.square_area, name='square_area'),
+    path('square', views.square),
     path('circle/<int:radius>', views.circle_area, name='circle_area'),
+    path('circle', views.circle),
     path('get_rectangle_area/<int:width>/<int:height>', views.get_rectangle_area),
     path('get_square_area/<int:width>', views.get_square_area),
     path('get_circle_area/<int:radius>', views.get_circle_area),
--- my_page/settings.py
+++ my_page/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'horoscope',
     'week_days',
+    'geometry',
 ]
 
 MIDDLEWARE = [
